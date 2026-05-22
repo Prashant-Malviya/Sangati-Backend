9f4c72d0635a3ba2cbf04760aab872d7882230e1
Prashant <prashantmalviya272002@gmail.com>
2026-05-22 05:34:22 +0530

    error handling

---
+++ src/util/error.ts
@@ -0,0 +1,22 @@
+import {Response} from "express";
+
+interface ErrorMessage extends Error {
+  status?: number;
+}
+
+export const TryError = (message: string, status: number = 500) => {
+  const error: ErrorMessage = new Error(message);
+
+  error.status = status;
+
+  return error;
+};
+
+export const CatchError = (error: unknown, res: Response, prodMessage:string = "internal server error") => {
+  if (error instanceof Error) {
+
+    const message = (process.env.NODE_ENV === "dev" ? error.message : prodMessage)
+    const status = (error as ErrorMessage).status || 500
+    res?.status(status).json({ message: message });
+  }
+};
