a701c6a5b69deccf572692d828f28e7435785f95
Prashant <prashantmalviya272002@gmail.com>
2026-06-02 02:39:13 +0530

    logout api created

--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getSession, refreshToken, signup, updateProfilePicture } from "../controller/auth.controller";
+import { getSession, logout, refreshToken, signup, updateProfilePicture } from "../controller/auth.controller";
 import { login } from "../controller/auth.controller";
 import AuthMiddlware from "../middleware/auth.middleware";
 import RefreshToken from "../middleware/refresh.middleware";
@@ -9,6 +9,7 @@ const AuthRouter = Router()
 
 AuthRouter.post("/signup", signup);
 AuthRouter.post("/login",login);
+AuthRouter.post("/logout",logout);
 AuthRouter.get("/refresh-token", RefreshToken,refreshToken);
 AuthRouter.get("/session", getSession)
 AuthRouter.put("/profile-picture", AuthMiddlware, updateProfilePicture)
