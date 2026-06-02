740e2e81873c95d522108e8fec9a9d114bd4a084
Prashant <prashantmalviya272002@gmail.com>
2026-06-03 05:07:38 +0530

    friend suggestion engine continue

---
+++ src/controller/friend.controller.ts
@@ -0,0 +1,46 @@
+import { Response } from "express"
+import { CatchError } from "../util/error"
+import FriendModel from "../model/friend.model"
+import { SessionInterface } from "../middleware/auth.middleware"
+import AuthModel from "../model/auth.model"
+
+export const addFriend = async(req: SessionInterface, res: Response)=>{
+    try {
+
+        req.body.user = req.session?.id
+        
+        const friend = await FriendModel.create(req.body)
+        res.json(friend)
+
+    } catch (error) {
+        
+        CatchError(error, res, "Failed to send friend request")
+    }
+}
+
+export const fetchFriends = async (req: SessionInterface, res: Response)=>{
+
+    try {
+        
+        const user = req.session?.id;
+        const friends = await FriendModel.find({user})
+
+        res.json(friends);
+
+    } catch (error) {
+        
+        CatchError(error,res,"Failed to send friend request")
+    }
+}
+
+export const suggestedFriends = async (req: SessionInterface, res: Response)=>{
+    try {
+        
+       const friends = await AuthModel.find()
+       res.json(friends);
+
+    } catch (error) {
+        
+        CatchError(error, res, "Failed to send friend request")
+    }
+}
